e6b1e297b4379f3e5121d155cffa5fca3be5daa1
BattleZone-Esport <nxxznesports@gmail.com>
2025-09-05 16:08:29 +0530

    requirements updated

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Rust install
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-# Force cargo cache to HOME
-export CARGO_HOME=$HOME/.cargo
-export RUSTUP_HOME=$HOME/.rustup
-
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
